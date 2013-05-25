1d981f17735f47f45c77ba148aabc86b1f7290ff
kentaurus44 <rigil44@gmail.com>
2013-05-24 23:13:06 -0400

    GUI
    22:30 - 23:10
    1) MapPanel.java
    - Map should be showing the correct data
    
    Note: Map does not get the filtered information.
    Everything should be working as they should be

--- src/src/MapPanel.java
+++ src/src/MapPanel.java
@@ -10,14 +10,16 @@ public class MapPanel extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawLine(250, 0, 250, 500);
-		g.drawLine(0, 250, 500, 250);
+		int wWidth = getWidth();
+		int wHeight = getHeight();
+		g.drawLine(wWidth/2, 0, wWidth/2, wHeight);
+		g.drawLine(0, wHeight/2, wWidth, wHeight/2);
 		final Object[][] obj = rs.getData();
 		for (int i=0; i <rs.getCount();i++) {
-			int j = (int) Math.ceil((Double)obj[i][2]/20);
-			int k = (int) Math.ceil((Double)obj[i][3]/20);
-			g.drawOval(j+250, k+250, 1, 1);
-			g.drawLine(j+250, k+250, 250, 250);
+			int j = (int) Math.ceil((Double)obj[i][2]) * wWidth/ 2  / rs.getRange() + wWidth/2;
+			int k = (int) Math.ceil((Double)obj[i][3]) * wHeight/-2 / rs.getRange() + wHeight/2;
+			g.drawOval(j, k, 1, 1);
+			g.drawLine(j, k, wWidth/2, wHeight/2);
 		}
 	}
 
