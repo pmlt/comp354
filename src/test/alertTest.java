84763838bf334456c57a48445f3458574d1d6147
Christopher Mukherjee <chris.mukherjee@gmail.com>
2013-05-27 01:09:42 -0400

    Added JUnit layout for test cases
    Will fill in rest of code when methods are available to test with

---
+++ src/test/alertTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/*	Test that different alert levels change the color of the table row(s)
+ *  and the icon(s) on the radar to:
+ *	RED	for HIGH level risk,
+ *	YELLOW for LOW level risk,
+ *  and WHITE for NONE (no risk). */
+public class alertTest {
+
+	@Test
+	public void testAlerts() {
+		VMS tester = new VMS();
+		fail("Not yet implemented");
+	}
+
+}
