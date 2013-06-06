b9ff8a5736bb63bb3d3d743d98d7ec012e3d46f7
Christopher Mukherjee <chris.mukherjee@gmail.com>
2013-06-05 20:46:13 -0400

    Updated some test cases
    yessir

--- src/test/alertTest.java
+++ src/test/alertTest.java
@@ -12,16 +12,28 @@ public class alertTest {
 
 	@BeforeClass
 	public static void testSetup() {
+		// Vessel 1 and 2 should generate Low level risk (yellow)
+		Vessel vessel1 = new Vessel("01",1,0,0,0,0,0);
+		Vessel vessel2 = new Vessel("02",1,100,0,0,0,0);
+		// Vessel 3 and 4 should generate High level risk (red)
+		Vessel vessel3 = new Vessel("03",1,500,500,0,0,0);
+		Vessel vessel4 = new Vessel("04",1,525,500,0,0,0);
 		RadarSimulator rs = new RadarSimulator();
-		rs.addVessel(ve)
-		VMS tester = new VMS();
-		tester.update(rs)
+		rs.addVessel(vessel1);
+		rs.addVessel(vessel2);
+		rs.addVessel(vessel3);
+		rs.addVessel(vessel4);
+		VMS tester = new VMS(rs);
+		tester.update();
+		boolean[] a1 = new boolean[5];
+		for (int i = 0; 0 <= 4; i++)
+			a1[i] = true;
 	}
-	
+		
 	@Test
-	public void testAlerts(rs) {
-		TablePanel table = new TablePanel();
-		assertEquals("High Risk = Red", 50, table.update(rs));
+	public void testAlerts() {
+		// Table color not implemented yet
+		// Test map color coding:
 	}
 
 }
--- src/test/maxVesselTest.java
+++ src/test/maxVesselTest.java
@@ -2,6 +2,8 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+
 /*	Test that only 100 ships can be present in the radar at one time.
  *  If 100 ships are present, new ships should be rejected. */
 public class maxVesselTest {
@@ -10,10 +12,12 @@ public class maxVesselTest {
 	public void test() {
 		RadarSimulator rs = new RadarSimulator();
 		// Add 100 ships to reach MAX_BOATS
-		for (int i = 1; i <= 100; i++)
+		for (int i = 1; i <= 100; i++) {
+			Vessel ve = new Vessel(i.toString(),1,0,0,0,0,0);
 			rs.addVessel(ve);
+		}
 		// Add another ship (101 total)
-	    assertEquals("Should reject and not add ship", , rs.addVessel(ve));
+	    assertEquals("Should be added to queue, Not table or radar", queue[0] = veExtra, rs.addVessel(veExtra));
 	}
 
 }
