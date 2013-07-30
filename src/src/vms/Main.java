86585b08e2ea5dc751daad11456a9dc97a1373a3
Philippe Milot <philmilot@gmail.com>
2013-07-30 14:03:55 -0400

    Implement VMS main function

--- src/src/vms/Main.java
+++ src/src/vms/Main.java
@@ -1,6 +1,24 @@
 package vms;
 
+import java.io.IOException;
+import vms.gui.*;
+
 public class Main {
+	private static ConnectionServer _Server;
+	private static RadarMonitor _Monitor;
+	private static MainGUI _MainGUI;
+	
+	private static class CSThread extends Thread {
+		@Override
+		public void run() {
+			try {
+				_Server.start();
+			} catch (IOException e) {
+				System.out.println(
+					"ConnectionServer threw an exception: " + e.getMessage());
+			}
+		}
+	}
 
 	/**
 	 * @param args
@@ -9,14 +27,30 @@ public class Main {
 		// XXX TODO
 		// In order:
 		// Create ConnectionServer instance
+		_Server = new ConnectionServer();
+		
 		// Create RadarMonitor instance
+		_Monitor = new RadarMonitor();
+		
 		// Register RadarMonitor as observer of ConnectionServer
+		_Server.registerObserver(_Monitor);
+		
 		// Spin a new thread, and run ConnectionServer::start() in it.
+		Thread t = new CSThread();
+		t.start();
 		
 		// Create vms.gui.Login instance, show the window
+		//LoginGUI = new Login();
+		//LoginGUI.show();
+		
 		// After user has logged in, create vms.gui.RadarDisplay instance
+		_MainGUI = new MainGUI();
+		
 		// Register RadarDisplay instance as observer of RadarMonitor
+		_Monitor.registerObserver(_MainGUI);
+		
 		// Show RadarDisplay window
+		_MainGUI.start();
 	}
 
 }
