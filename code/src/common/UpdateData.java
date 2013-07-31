b9d5e4e5ccb38826cef4982fca70de3e0f45f2dc
gledoux <ghislain.ledoux@gmail.com>
2013-07-31 13:32:57 -0400

    Fixes to class UpdateData.java -Created inner class ParsedData to serialize/unserialize data without losing accuracy. -Throw JsonSyntaxException whenever an object field is not populated after unserialization.

--- code/src/common/UpdateData.java
+++ code/src/common/UpdateData.java
@@ -1,9 +1,9 @@
 package common;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
 import common.Vessel.VesselType;
-
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
@@ -14,6 +14,24 @@ public class UpdateData {
 	public Coord Coordinates;
 	public Course Course;
 	public Calendar Timestamp;
+	
+	private class ParsedData {
+		private String Id;
+		private VesselType Type;
+		private Coord Coordinates;
+		private Course Course;
+		private long Time;
+		private String Timezone;
+		
+		private ParsedData(String id, VesselType type, Coord coords, Course course, long time, String timezone) {
+			Id = id;
+			Type = type;
+			Coordinates = coords;
+			Course = course;
+			Time = time;
+			Timezone = timezone;
+		}
+	}
 	
 	public UpdateData() {
 	}
@@ -28,12 +46,19 @@ public class UpdateData {
 	
 	public String toJSON() {
 		Gson gson = new Gson();
-		return gson.toJson(this);
+		return gson.toJson(new ParsedData(Id, Type, Coordinates, Course, Timestamp.getTimeInMillis(),
+				Timestamp.getTimeZone().getID()));
 	}
 	
 	public static UpdateData fromJSON(String json) throws JsonSyntaxException {
 		Gson gson = new Gson();
-		return gson.fromJson(json, UpdateData.class);
+		ParsedData pd = gson.fromJson(json, ParsedData.class);
+		if (pd.Id == null || pd.Type == null || pd.Coordinates == null || pd.Course == null || pd.Time == 0 || pd.Timezone == null)
+			throw new JsonSyntaxException("Data was parsed incorrectly");
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(pd.Time);
+		cal.setTimeZone(TimeZone.getTimeZone(pd.Timezone));
+		return new UpdateData(pd.Id, pd.Type, pd.Coordinates, pd.Course, cal);
 	}
 	
 }
