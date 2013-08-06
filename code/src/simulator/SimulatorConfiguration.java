be8647d3aa29cb71fd6123952e78ebc5b0d9444d
Philippe Milot <philmilot@gmail.com>
2013-08-06 18:54:24 -0400

    Unit test for VSF parsing

--- code/src/simulator/SimulatorConfiguration.java
+++ code/src/simulator/SimulatorConfiguration.java
@@ -65,7 +65,7 @@ public class SimulatorConfiguration {
 	 * Should throw ParseException whenever it encounters an unexpected
 	 * format in the input file.
 	 */
-	public static SimulatorConfiguration parseVSF(BufferedReader in) 
+	public static SimulatorConfiguration parseVSF(BufferedReader in, Calendar reftime) 
 			throws IOException, ParseException {
 		SimulatorConfiguration sc = new SimulatorConfiguration();
 		String line;
@@ -90,7 +90,7 @@ public class SimulatorConfiguration {
 					v = new Vessel(a[1], VesselType.values()[5]);
 				v.update(new Coord(Double.parseDouble(a[3]), Double.parseDouble(a[4])),
 						new Course(Double.parseDouble(a[5]), Double.parseDouble(a[6])),
-						Calendar.getInstance());
+						reftime);
 				sc.addVessel(v);
 			}
 			else if (a[0].trim().compareToIgnoreCase("NEWT") == 0 && Double.parseDouble(a[7]) > 0) {
@@ -101,13 +101,21 @@ public class SimulatorConfiguration {
 					v = new Vessel(a[1], VesselType.values()[5]);
 				v.update(new Coord(Double.parseDouble(a[3]), Double.parseDouble(a[4])),
 						new Course(Double.parseDouble(a[5]), Double.parseDouble(a[6])),
-						Calendar.getInstance());
+						reftime);
 				sc.addVessel(v);
 			}
 			
 		} // END OF WHILE LOOP
 		
-		//
+		// Check that data was correct.
+		if (!"001".equals(sc._Version) || 
+			sc._RadarRange < 0 ||
+			sc._StartDelay < 0 ||
+			sc._TimeInterval < 0.5 ||
+			sc._TotalTime < (sc._TimeInterval) ||
+			sc._Vessels.size() <= 0) {
+			throw new ParseException("VSF data incomplete or incorrect!", 0);
+		}
 		return sc;
 	}
 }
