f18e48b87d793491ba773a67f36b222c43f9a435
Stefanie Lavoie <lavoie.stefanie@hotmail.com>
2013-08-06 19:50:09 -0400

    fix vessels not getting removed

--- code/src/common/Vessel.java
+++ code/src/common/Vessel.java
@@ -112,4 +112,11 @@ public class Vessel {
 		
 		return new UpdateData(id, type, tempCoords, tempCourse, timestamp);
 	}
+	
+	public boolean equals(Object other){
+		Vessel vOther = (Vessel)other;
+		if(id.equals(vOther.getId()) && type == vOther.getType())
+			return true;
+		return false;
+	}
 }
