a6f53967d3810828eccbd3c456ee3000ebfb3911
Christopher Mukherjee <chris.mukherjee@gmail.com>
2013-08-09 03:06:37 -0400

    Changes to login
    See email

--- code/src/vms/gui/Login.java
+++ code/src/vms/gui/Login.java
@@ -8,47 +8,59 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JRootPane;
-import javax.swing.JTextField;
 
 public class Login implements ActionListener, WindowListener {
 	MainGUI _Main;
 	JFrame log;
-	JTextField pass;
+	JComboBox comboBox;
+	JLabel label1;
+	JPasswordField pass;
+	JLabel label2;
 	JButton but[] = {new JButton("Login"), new JButton("Exit")};
 	public Login (MainGUI main) {
 		_Main = main;
 		log = new JFrame("Login");
 		log.addWindowListener(this);
-		log.setSize(200, 140);
+		log.setSize(250, 150);
 		log.setResizable(false);
 		log.setLocationRelativeTo(null);
 		log.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 		//Creating fields
-		JLabel label = new JLabel("Enter your password");
-//		JLabel op = new JLabel("Operator: op123");
-//		JLabel us = new JLabel("User: user123");
-		pass = new JTextField();
+		DefaultComboBoxModel users = new DefaultComboBoxModel(new String[]{"user","operator"});
+		comboBox = new JComboBox(users);
+		label1 = new JLabel("username:");
+		label2 = new JLabel("password:");
+//		JLabel ad = new JLabel("Operator: op123");
+		pass = new JPasswordField();
 		
 		JPanel output = new JPanel();
 		output.setLayout(new GridLayout(3,1));		
 		JPanel commands = new JPanel();
 		
 		// Creating functions
+		comboBox.addActionListener(this);
 		but[0].addActionListener(this);
 		but[1].addActionListener(this);
 		// Adding fields to panels
 //		output.add(op);
 //		output.add(us);
-		output.add(label);
+		output.add(label1);
+		output.add(comboBox);
+		output.add(label2);
 		output.add(pass);
-		
+		label2.setEnabled(false);
+		pass.setEnabled(false);
+
 		commands.add(but[0]);
 		commands.add(but[1]);
 		// Adding Panels to Frame
@@ -67,11 +79,11 @@ public class Login implements ActionListener, WindowListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource().equals(but[0])) {
-			if (pass.getText().compareTo("op123") == 0) {
+			if (comboBox.getSelectedItem().toString() == "operator" && pass.getText().compareTo("op123") == 0) {
 				_Main.showRadar(MainGUI.UserIdentity.OPERATOR);
 				log.setVisible(false);
 			}
-			else if (pass.getText().compareTo("user123") == 0) {
+			else if (comboBox.getSelectedItem().toString() == "user") {
 				_Main.showRadar(MainGUI.UserIdentity.NORMAL_USER);
 				log.setVisible(false);
 			}
@@ -79,6 +91,19 @@ public class Login implements ActionListener, WindowListener {
 		else if (e.getSource().equals(but[1])) {
 			close();
 		}
+		else if (e.getSource().equals(comboBox)) {
+			if (comboBox.getSelectedItem().toString() == "operator") {
+				label2.setEnabled(true);
+				pass.setEnabled(true);
+			}
+			else if (comboBox.getSelectedItem().toString() == "user") {
+				label2.setEnabled(false);
+				pass.setEnabled(false);
+				pass.setText("");
+			}
+
+		}
+
 	}
 	
 	public void close() {
